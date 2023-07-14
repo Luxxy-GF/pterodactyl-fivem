93fbf63d736de015b397b7fd1c21a4dd6d0a4bd3
Luxxy Seed <deniedaddicted@gmail.com>
2023-07-14 15:12:47 +0000

    eeeeeeeeeeee

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,22 +1,11 @@
 #!/bin/bash
-# Default the TZ environment variable to UTC.
-TZ=${TZ:-UTC}
-export TZ
+sleep 2
 
-# Set environment variable that holds the Internal Docker IP
-INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
-export INTERNAL_IP
+cd /home/container
 
-# Switch to the container's working directory
-cd /home/container || exit 1
+# Replace Startup Variables
+MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+echo ":/home/container$ ${MODIFIED_STARTUP}"
 
-# Convert all of the "{{VARIABLE}}" parts of the command into the expected shell
-# variable format of "${VARIABLE}" before evaluating the string and automatically
-# replacing the values.
-PARSED=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat -)")
-
-# Display the command we're running in the output, and then execute it with the env
-# from the container itself.
-printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0m%s\n" "$PARSED"
-# shellcheck disable=SC2086
-exec env ${PARSED}
+# Run the Server
+${MODIFIED_STARTUP}
