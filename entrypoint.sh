e26bbbefbae645b5b633a5a46c690094e62235b4
Luxxy Seed <deniedaddicted@gmail.com>
2023-07-14 15:21:39 +0000

    gey

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,10 +8,6 @@ RESET_COLOR='\033[0m'
 # Set environment variable that holds the Internal Docker IP
 INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
 export INTERNAL_IP
-
-# Print Node.js Version
-node -v
-
 # Replace Startup Variables
 # shellcheck disable=SC2086
 echo -e "${CYAN}STARTUP /home/container: /start.sh ${RESET_COLOR}"
