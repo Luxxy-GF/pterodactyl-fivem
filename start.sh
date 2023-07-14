7280604702063a2123fc4b9e692b0f7f203caf89
Luxxy Seed <deniedaddicted@gmail.com>
2023-07-14 15:57:52 +0000

    e

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "hi"
+echo "${AUTO_UPDATE}"
 RELEASE_PAGE=$(curl -sSL https://runtime.fivem.net/artifacts/fivem/build_proot_linux/master/)
 CHANGELOGS_PAGE=$(curl -sSL https://changelogs-live.fivem.net/api/changelog/versions/linux/server)
 
