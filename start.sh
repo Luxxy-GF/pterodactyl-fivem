84458b62131ef7740a8a661245b5906cb77a5d68
Luxxy <68338577+Luxxy-GF@users.noreply.github.com>
2024-03-04 00:27:45 +0000

    Max Clients

--- start.sh
+++ start.sh
@@ -29,5 +29,5 @@ fi
 echo -e "${GREEN}Starting FiveM Binary...${NC} Have Fun!"
 echo -e "${GREEN}This Egg was made by Luxxy-GF${NC}"
 echo -e "${RED} Github: Luxxy-GF | Discord: LuxxyWasTaken${NC}"
-$(pwd)/alpine/opt/cfx-server/ld-musl-x86_64.so.1 --library-path "$(pwd)/alpine/usr/lib/v8/:$(pwd)/alpine/lib/:$(pwd)/alpine/usr/lib/" -- $(pwd)/alpine/opt/cfx-server/FXServer +set citizen_dir $(pwd)/alpine/opt/cfx-server/citizen/ +set sv_licenseKey ${FIVEM_LICENSE} +set steam_webApiKey ${STEAM_WEBAPIKEY} +set sv_maxplayers ${MAX_PLAYERS} +set serverProfile ${TXADMIN_PROFILE} +set txAdminPort ${TXADMIN_PORT}  $( [ "$TXADMIN_ENABLE" == "1" ] || printf %s '+exec server.cfg' )
+$(pwd)/alpine/opt/cfx-server/ld-musl-x86_64.so.1 --library-path "$(pwd)/alpine/usr/lib/v8/:$(pwd)/alpine/lib/:$(pwd)/alpine/usr/lib/" -- $(pwd)/alpine/opt/cfx-server/FXServer +set citizen_dir $(pwd)/alpine/opt/cfx-server/citizen/ +set sv_licenseKey ${FIVEM_LICENSE} +set steam_webApiKey ${STEAM_WEBAPIKEY} +set sv_maxclients ${MAX_PLAYERS} +set serverProfile ${TXADMIN_PROFILE} +set txAdminPort ${TXADMIN_PORT}  $( [ "$TXADMIN_ENABLE" == "1" ] || printf %s '+exec server.cfg' )
 ## FIVEM_LICENSE, STEAM_WEBAPIKEY, MAX_PLAYERS, TXADMIN_PROFILE, TXADMIN_PORT, TXADMIN_ENABLE
