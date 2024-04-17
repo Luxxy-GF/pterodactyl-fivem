04fa9f80dc1834544b719208a9debd7c28689487
Luxxy-GF <deniedaddicted@gmail.com>
2024-04-17 11:57:19 +0000

    Bug FIX

--- start.sh
+++ start.sh
@@ -25,6 +25,7 @@ else
 fi
 
 echo -e "${GREEN}Starting FiveM Binary...${NC} Have Fun!"
-echo -e "${RED}If you have any issues please contact us on discord: https://discord.gg/pterodactyl${NC}"
+echo -e "${RED}If you have any issues please contact me in the discord: https://discord.gg/pelican-panel${NC}"
 echo -e "${RED} Github: Luxxy-GF | Discord: LuxxyWasTaken${NC}"
-$(pwd)/alpine/opt/cfx-server/ld-musl-x86_64.so.1 --library-path "$(pwd)/alpine/usr/lib/v8/:$(pwd)/alpine/lib/:$(pwd)/alpine/usr/lib/" -- $(pwd)/alpine/opt/cfx-server/FXServer +set citizen_dir $(pwd)/alpine/opt/cfx-server/citizen/ +set serverProfile default +set txAdminPort ${TXADMIN_PORT}
+
+$(pwd)/alpine/opt/cfx-server/ld-musl-x86_64.so.1 --library-path "$(pwd)/alpine/usr/lib/v8/:$(pwd)/alpine/lib/:$(pwd)/alpine/usr/lib/" -- $(pwd)/alpine/opt/cfx-server/FXServer +set citizen_dir $(pwd)/alpine/opt/cfx-server/citizen/ +set sv_licenseKey {{FIVEM_LICENSE}} +set steam_webApiKey {{STEAM_WEBAPIKEY}} +set sv_maxplayers {{MAX_PLAYERS}} +set serverProfile default +set txAdminPort {{TXADMIN_PORT}} $( [ "$TXADMIN_ENABLE" == "1" ] || printf %s '+exec server.cfg' )
