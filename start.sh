0f5776e0c709ff91fc277f0f27a7b890509f3104
Luxxy-GF <deniedaddicted@gmail.com>
2024-04-17 12:05:57 +0000

    i mean more bug fix from the last bug fix

--- start.sh
+++ start.sh
@@ -28,4 +28,4 @@ echo -e "${GREEN}Starting FiveM Binary...${NC} Have Fun!"
 echo -e "${RED}If you have any issues please contact me in the discord: https://discord.gg/pelican-panel${NC}"
 echo -e "${RED} Github: Luxxy-GF | Discord: LuxxyWasTaken${NC}"
 
-$(pwd)/alpine/opt/cfx-server/ld-musl-x86_64.so.1 --library-path "$(pwd)/alpine/usr/lib/v8/:$(pwd)/alpine/lib/:$(pwd)/alpine/usr/lib/" -- $(pwd)/alpine/opt/cfx-server/FXServer +set citizen_dir $(pwd)/alpine/opt/cfx-server/citizen/ +set sv_licenseKey {{FIVEM_LICENSE}} +set steam_webApiKey {{STEAM_WEBAPIKEY}} +set sv_maxplayers {{MAX_PLAYERS}} +set serverProfile default +set txAdminPort {{TXADMIN_PORT}} $( [ "$TXADMIN_ENABLE" == "1" ] || printf %s '+exec server.cfg' )
+$(pwd)/alpine/opt/cfx-server/ld-musl-x86_64.so.1 --library-path "$(pwd)/alpine/usr/lib/v8/:$(pwd)/alpine/lib/:$(pwd)/alpine/usr/lib/" -- $(pwd)/alpine/opt/cfx-server/FXServer +set citizen_dir $(pwd)/alpine/opt/cfx-server/citizen/ +set sv_licenseKey ${FIVEM_LICENSE} +set steam_webApiKey ${STEAM_WEBAPIKEY} +set sv_maxplayers ${MAX_PLAYERS} +set serverProfile default +set txAdminPort ${TXADMIN_PORT} $( [ "$TXADMIN_ENABLE" == "1" ] || printf %s '+exec server.cfg' )
