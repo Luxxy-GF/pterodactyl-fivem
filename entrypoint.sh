22908c167ce8a04284cbaefd4d073107f6eff64e
default <deniedaddicted@gmail.com>
2024-05-17 21:41:08 +0000

    💀

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
 cd /home/container || exit 1
-
-# Configure colors
-CYAN='\033[0;36m'
-RESET_COLOR='\033[0m'
-
-# Replace Startup Variables
-# shellcheck disable=SC2086
-echo -e "${CYAN}STARTUP /home/container: /start.sh ${RESET_COLOR}"
-# Run the Server
-# shellcheck disable=SC2086
-
 eval '/start.sh'
